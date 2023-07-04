224191138cccdb65f4cd02023fcedc545c450253
willbastidas <willkergaciab@gmail.com>
2023-07-04 16:46:30 -0400

    practica de componentes

---
+++ componentes/boton.js
@@ -0,0 +1,15 @@
+import {TouchableOpacity, Text} from "react-native"
+
+export function Btn () {
+    return (
+    <>
+        <TouchableOpacity>
+            <Text>Boton</Text>
+        </TouchableOpacity>
+    
+    
+    </>
+    )
+}
+
+
