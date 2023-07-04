0a239d39898ce6be1f0ec7842f9c4783bf6cf906
willbastidas <willkergaciab@gmail.com>
2023-07-04 15:46:31 -0400

    ejemplo de contador

---
+++ ejemplo_contador.js
@@ -0,0 +1,26 @@
+// import { StatusBar } from 'expo-status-bar';
+// import React, { useState } from 'react';
+// import { StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native';
+// import styles from "util/estilos";
+
+
+// export default function App() {
+
+//   const [contador, setContador] = useState(0);
+//   const aumentarCont = () => {
+//     const nuevoValor = contador + 1;
+//     setContador (nuevoValor);
+//   }
+
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.estilosTexto}>Vamos a contar!!</Text>
+//       <Text style={styles.estiloContador}>{contador}</Text>
+//       <StatusBar style="auto" />
+//       {/* <TextInput style={styles.textInput} placeholder='prueba de texto'/> */}
+//       <TouchableOpacity styles={styles.btnAlerta} onPress={aumentarCont} >
+//         <Text style={styles.btnAlert}>sumar de uno en 1</Text>
+//       </TouchableOpacity>
+//     </View>
+//   );
+// }
