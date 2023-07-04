74ece5991e5304cfff3d34ece63d1e822d884104
willbastidas <willkergaciab@gmail.com>
2023-07-04 15:44:40 -0400

    contador de prueba
    contador de prueba para probar funcionades

--- App.js
+++ App.js
@@ -1,17 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native';
 import styles from "util/estilos";
 
 
 export default function App() {
+
+  const [contador, setContador] = useState(0);
+  const aumentarCont = () => {
+    const nuevoValor = contador + 1;
+    setContador (nuevoValor);
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.estilosTexto}>prueba de app</Text>
+      <Text style={styles.estilosTexto}>Vamos a contar!!</Text>
+      <Text style={styles.estiloContador}>{contador}</Text>
       <StatusBar style="auto" />
-      <TextInput style={styles.textInput} placeholder='prueba de texto'/>
-      <TouchableOpacity styles={styles.btnAlerta} onPress={()=>{alert("Haz tocado el boton")}} >
-        <Text style={styles.btnAlert}>precaución</Text>
+      {/* <TextInput style={styles.textInput} placeholder='prueba de texto'/> */}
+      <TouchableOpacity styles={styles.btnAlerta} onPress={aumentarCont} >
+        <Text style={styles.btnAlert}>sumar de uno en 1</Text>
       </TouchableOpacity>
     </View>
   );
--- utilidades/estilos.js
+++ utilidades/estilos.js
@@ -1,6 +1,11 @@
 import { StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
+  estiloContador: {
+    marginTop: 5,
+    marginBottom: 5, 
+    fontSize: 40
+  },
   colorTextBtn: {
     color: "#fff"
   },
